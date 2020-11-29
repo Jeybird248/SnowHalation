8114aa53f5f5baee6e7ba6a5df1538816629a5f5
matthewfloress <70657287+matthewfloress@users.noreply.github.com>
2020-11-29 11:59:49 -0800

    Update and rename tree.java to snowmantree.java

--- snowmantree.java
+++ snowmantree.java
@@ -1,8 +1,17 @@
+penColor(rgb(224, 255, 255));
+dot(1000);
+drawAllSnowmen();
 drawAllTree();
 function drawAllTree() {
   for (var i = 0; i < 3; i++) {
     moveTo(randomNumber(0, 310), 320);
     drawTree(randomNumber(50, 230));
+  }
+}
+function drawAllSnowmen() {
+  for (var i = 0; i < 4; i++) {
+    moveTo(randomNumber(0, 310), 445);
+    drawSnowman(randomNumber(10, 50));
   }
 }
 function drawTree(size) {
@@ -27,4 +36,26 @@ function drawTree(size) {
   turnLeft(90);
   moveForward(size);
   penUp();
-  }
+  
+}
+function drawSnowman(size) {
+//snow balls
+penDown();
+
+penColor(rgb(255, 255, 255));
+dot(size);
+moveForward(size);
+dot(size);
+moveForward(size);
+dot(size);
+moveForward(size);
+penColor(rgb(255, 165, 0));
+turnRight(120);
+moveForward(size);
+turnRight(120);
+moveForward(size);
+turnRight(120);
+moveForward(size);
+penUp();
+
+}
