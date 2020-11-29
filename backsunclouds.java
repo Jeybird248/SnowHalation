66dc28770f23f8d63efcdecba066dc027c7deef0
ewikpark <70724844+ewikpark@users.noreply.github.com>
2020-11-28 22:32:32 -0800

    Update backsunclouds.java

--- backsunclouds.java
+++ backsunclouds.java
@@ -24,11 +24,12 @@ function drawAllClouds(size){
 }
 //This makes sure that the cloud's position is randomized. 
 function drawCloud(size){
-  moveTo(randomNumber(25,250), randomNumber(25,210));
+  moveTo(randomNumber(25,200), randomNumber(25,200));
   penRGB(255,255,255,1);
   dot(size);
   //This will give the cloud its shape
   for(var i=0; i<2; i++){
+  //This loop will essentially demonstrate that there will be no more than one cloud.
     turnTo(300);
     moveForward(size);
     dot(size);
