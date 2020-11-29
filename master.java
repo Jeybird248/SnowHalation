51fbfb638a8df193ebec77619ef6205eeb14e042
Jehyeok Yeon (Tommy) <69325815+Jeybird248@users.noreply.github.com>
2020-11-29 12:15:55 -0800

    Update master.java

--- master.java
+++ master.java
@@ -3,8 +3,8 @@ penUp();
 //draws the background
 Background();
 var colors = ["lightblue","darkblue","red", "white", "black", "lightyellow"];
-
-
+drawAllSnowmen();
+drawAllTree();
 //draws 50 snowflakes 
 for (var i = 0; i < 25; i++) {
   //uses random size and thickness
@@ -42,10 +42,10 @@ function Background(){
   penColor("lightblue");
   dot(360);
   penUp();
-  drawAllClouds(20, 20);
+  drawAllClouds(20);
   //This part is for the Sun.
   penColor("yellow");
-   moveTo(280, 40);
+  moveTo(280, 40);
   dot(30);
   penUp();
   moveTo(0, 360);
@@ -55,7 +55,7 @@ function Background(){
 //This calls the Cloud function, which will essentially ensure that the clouds will appear.
 function drawAllClouds(size){
   for (var i = 0; i < 4; i++) {
-    drawCloud(randomNumber(size));
+    drawCloud(size);
   }
 }
 //This makes sure that the cloud's position is randomized. 
@@ -73,4 +73,61 @@ function drawCloud(size){
     moveForward(size);
     dot(size);
   }
+}
+function drawAllTree() {
+  for (var i = 0; i < 3; i++) {
+    moveTo(randomNumber(0, 310), 320);
+    drawTree(randomNumber(50, 230));
+  }
+}
+function drawAllSnowmen() {
+  for (var i = 0; i < 4; i++) {
+    moveTo(randomNumber(0, 310), 445);
+    drawSnowman(randomNumber(10, 50));
+  }
+}
+function drawTree(size) {
+  penColor(rgb(0,128,0));
+  penDown();
+  penWidth(25);
+  turnTo(270);
+  
+    //tree leaves
+  moveForward(size);
+  turnRight(120);
+  moveForward(size);
+  turnRight(120);
+  moveForward(size);
+  turnRight(120);
+  penUp();
+    //bark
+  penDown();
+  moveForward(size/2);
+  penColor(rgb(210,105,30));
+  penWidth(25);
+  turnLeft(90);
+  moveForward(size);
+  penUp();
+  
+}
+function drawSnowman(size) {
+  //snow balls
+  penDown();
+  penWidth(5);
+  penColor(rgb(255, 255, 255));
+  dot(size);
+  moveForward(size);
+  dot(size);
+  moveForward(size);
+  dot(size);
+  moveForward(size);
+  penColor(rgb(255, 165, 0));
+  turnRight(120);
+  moveForward(size);
+  turnRight(120);
+  moveForward(size);
+  turnRight(120);
+  moveForward(size);
+  penUp();
+
 }
